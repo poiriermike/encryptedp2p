051e3a062cbd1e3edf23f08fe14e531207b7475c
Mike Poirier <mpoirier@uvic.ca>
2015-02-22 11:30:41 -0800

    Added a more robust client and server node to the main folder.

---
+++ dedicated-server.py
@@ -0,0 +1,41 @@
+import sys, os
+# Uses local version of Kademlia
+sys.path.insert(0, "kademlia")
+from twisted.internet import reactor
+from twisted.python import log
+from kademlia.network import Server
+import argparse
+
+# This is a really simple dedicated server. It will start on the specified port, load state variables from a file, and
+# save them intermittently.
+
+backup = "server_state.bak"
+
+# Some fancy argument parsing. cause I'm cool like that.
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--port', dest='port', type=int, required=True, action='store', help='Port to start the server on. Defaults to 6060.')
+parser.add_argument('-l', '--log', dest='log', type=str, action='store', default=False, help='Specify a log file to output to. Default is stdout.')
+args = parser.parse_args()
+
+
+# Logging and fun stuff like that
+if args.log:
+    l = open(args.log, "a")
+    log.startLogging(l)
+else:
+    log.startLogging(sys.stdout)
+
+print("Setting up listening server")
+server = Server()
+server.listen(args.port)
+
+# This is a backup
+if os.path.isfile(backup):
+    server.loadState(backup)
+
+# Saves to a backup every 5 minutes
+if os.path.exists(backup):
+    server.saveStateRegularly(backup, 300)
+
+reactor.run()
+
