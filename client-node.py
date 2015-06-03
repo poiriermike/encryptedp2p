1782cabef3a515bb6e819e7598c6aeb8ce8ce94f
Robert <drumheadjr@gmail.com>
2015-06-02 17:35:23 -0700

    first 'running' version of gui in seperate class. seems some functionality has been broken

--- client-node.py
+++ client-node.py
@@ -141,184 +141,196 @@ from twisted.internet.protocol import Factory, ClientFactory, ServerFactory, Pro
 from twisted.internet import protocol, reactor, stdio
 from twisted.protocols import basic
 
-# list boxes in GUI for displaying and selecting contact info
-ConnectionsList = []
+class client_gui:
+    # list boxes in GUI for displaying and selecting contact info
+    ConnectionsList = []
 
-chatWindow = NONE
-textEntry = NONE
+    chatWindow = NONE
+    textEntry = NONE
 
-# print givent text in the chat text window
-def chatWindowPrintText(text):
-    chatWindow.config(state=NORMAL)
-    chatWindow.insert(END, text)
-    chatWindow.config(state=DISABLED)
-    chatWindow.see(END)
+    #class constructor
+    def __init__(self, reactor):
+        self.reactor = reactor
 
-# Hopefully temp way to clean up the newline in the text box after sending messages
-def clearText(event):
-    if event.keysym == 'Return':
-        textEntry.delete('0.0', END)
+    # print givent text in the chat text window
+    def chatWindowPrintText(self, text):
+        chatWindow.config(state=NORMAL)
+        chatWindow.insert(END, text)
+        chatWindow.config(state=DISABLED)
+        chatWindow.see(END)
 
-#Send a message through the GUI chat
-def sendChatMessage(event):
-    global textEntry
-    global clientFactory
+    # Hopefully temp way to clean up the newline in the text box after sending messages
+    def clearText(self, event):
+        if event.keysym == 'Return':
+            textEntry.delete('0.0', END)
 
-    if event.keysym == 'Return':
-        selectedContact = updateSelectedContact()
-        
-        if(selectedContact == None):
-            chatWindowPrintText("No Contact Selected\n")
-            return False
+    #Send a message through the GUI chat
+    def sendChatMessage(self, event):
+        global textEntry
+        global clientFactory
 
-        selectedIP = selectedContact['ip']
-        selectedPort = selectedContact['port']
+        if event.keysym == 'Return':
+            selectedContact = updateSelectedContact()
 
-        if selectedIP is NONE or selectedPort is None:
-            return False
+            if(selectedContact == None):
+                chatWindowPrintText("No Contact Selected\n")
+                return False
 
-        message = textEntry.get('0.0', END)
-        textEntry.delete('0.0', END)
+            selectedIP = selectedContact['ip']
+            selectedPort = selectedContact['port']
 
-        message = message.lstrip()
-        if message != "":
-            chatWindowPrintText("Me: "+message)
-            log.msg("Client Send: " + message)
-            server.sendMessage(username + ": " + message, selectedIP, selectedPort)
+            if selectedIP is NONE or selectedPort is None:
+                return False
 
+            message = textEntry.get('0.0', END)
+            textEntry.delete('0.0', END)
+
+            message = message.lstrip()
+            if message != "":
+                chatWindowPrintText("Me: "+message)
+                log.msg("Client Send: " + message)
+                server.sendMessage(username + ": " + message, selectedIP, selectedPort)
+
+    # Takes the result from the DHT and parses out the IP and port
+    # TODO: This will have to be modified when we have to resolve multiple IP/PORT pairs for NAT etc.
+    def get_contact_location(self, result, contact):
+        if result is not None and result != []:
+            contact['ip'] = result[0][0]
+            contact['port'] = result[0][1]
+
+    # Refreshes the IPs of all of the contacts. Because of async nature of Twisted, this may not show right away.
+    def refreshAvailIP(self):
+        global Contacts
+        log.msg("Refreshing Contact List automagically")
+        for contact in Contacts:
+            # This adds the get_ip function to the server callback list. Will do so for each contact
+            #server.get(contact['key'] + contact['username']).addCallback(get_contact_location, contact)
+            server.getContactInfo(contact['key'] + contact['username'], contact['key']).\
+                addCallback(get_contact_location, contact)
+
+        #clear the listboxes in the GUI of old values
+        ConnectionsList[0].delete(0, END)
+        ConnectionsList[1].delete(0, END)
+
+        for contact in Contacts:
+            contactName = contact['username']
+            contactIP = contact['ip']
+            if contactIP is "" or contactIP is None:
+                contactIP = "Offline"
+
+            # add the new values to the GUI
+            ConnectionsList[0].insert(END, contactName)
+            ConnectionsList[1].insert(END, contactIP)
+
+    # update the global selected IP address
+    def updateSelectedContact(self):
+
+        selectedName = ConnectionsList[0].get(ACTIVE)
+        selectedIP = ConnectionsList[1].get(ACTIVE)
+
+        for contact in Contacts:
+            if contact['ip'] == selectedIP and contact['username'] == selectedName:
+                return contact
+        return None
+
+    #method is called by a ConnectionsList list box when the selection is changed. Updates other accordingly
+    def syncListSelections(self, evt=None, listIndex=0):
+
+        otherindex = (listIndex +1) % 2
+        toset = ConnectionsList[listIndex].curselection()
+        ConnectionsList[otherindex].activate(toset[0])
+
+    def closeProgram(self):
+        reactor.stop()
+
+    #Set up the GUI and containers, frames, lists, etc. before running the program loop
+    def initializeGUI(self):
+        global ConnectionsList
+        global chatWindow
+        global textEntry
+        #set up the main window
+        root = Tk()
+        root.title("Encrypted P2P Chat GUI")
+        root.protocol('WM_DELETE_WINDOW', self.closeProgram)
+
+        mainFrame = Frame(root)
+        mainFrame.pack()
+
+        self.ConnectionsList.append(Listbox(mainFrame, selectmode=SINGLE))
+        self.ConnectionsList.append(Listbox(mainFrame, selectmode=SINGLE))
+
+        self.ConnectionsList[0].grid(row=0, column=0)
+        self.ConnectionsList[1].grid(row=0, column=1)
+
+        self.ConnectionsList[0].bind("<<ListboxSelect>>", lambda e:syncListSelections(e, listIndex=0))
+        self.ConnectionsList[1].bind("<<ListboxSelect>>", lambda e:syncListSelections(e, listIndex=1))
+
+        #set up chat window with scroll bar
+        chatTextFrame = Frame(root)
+
+        scrollbar = Scrollbar(chatTextFrame)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        chatWindow = Text(chatTextFrame, height=8, state=DISABLED)
+        chatWindow.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        scrollbar.config(command=chatWindow.yview)
+        chatWindow.config(yscrollcommand=scrollbar.set)
+
+        chatTextFrame.pack(expand=YES, fill=BOTH)
+
+        #set up user text field for input
+        chatEntryFrame = Frame(root)
+
+        scrollbar2 = Scrollbar(chatEntryFrame)
+        scrollbar2.pack(side=RIGHT, fill=Y)
+
+        textEntry = Text(chatEntryFrame, height=2)
+        textEntry.pack(side=LEFT, expand=YES, fill=BOTH)
+
+        scrollbar2.config(command=textEntry.yview)
+
+        chatEntryFrame.pack(expand=YES, fill=BOTH)
+
+        #Bind key events to method calls
+        textEntry.bind("<Key>", self.sendChatMessage)
+        textEntry.bind("<KeyRelease>", self.clearText)
+
+
+        #set up buttons and their method calls
+        refreshButton = Button(root, text="Refresh List", command=self.refreshAvailIP)
+        refreshButton.pack(side=LEFT)
+
+        exitButton = Button(root, text="Exit Program", command=self.closeProgram)
+        exitButton.pack(side=RIGHT)
+
+        #connectButton = Button(root, text="Connect", command=None)#connectToIP)
+        #connectButton.pack(side=RIGHT)
+
+        return root
+
+gui = client_gui(reactor)
+
+#set up the gui root and connect it to the reactor
+if not args.nogui:
+    root = gui.initializeGUI()
+    tksupport.install(root)
+
+# --------- gui interaction --------
+
+# update gui when new message received
 def pollForMessage():
     messages = server.pollReceivedMessages()
-    
+
     if messages is None:
         return
 
     for message in messages:
         if message != "":
             log.msg("Server Recieved: " + message)
-            chatWindowPrintText(message)
-
-# Takes the result from the DHT and parses out the IP and port
-# TODO: This will have to be modified when we have to resolve multiple IP/PORT pairs for NAT etc.
-def get_contact_location(result, contact):
-    if result is not None and result != []:
-        contact['ip'] = result[0][0]
-        contact['port'] = result[0][1]
-
-# Refreshes the IPs of all of the contacts. Because of async nature of Twisted, this may not show right away.
-def refreshAvailIP():
-    global Contacts
-    log.msg("Refreshing Contact List automagically")
-    for contact in Contacts:
-        # This adds the get_ip function to the server callback list. Will do so for each contact
-        #server.get(contact['key'] + contact['username']).addCallback(get_contact_location, contact)
-        server.getContactInfo(contact['key'] + contact['username'], contact['key']).\
-            addCallback(get_contact_location, contact)
-
-    #clear the listboxes in the GUI of old values
-    ConnectionsList[0].delete(0, END)
-    ConnectionsList[1].delete(0, END)
-
-    for contact in Contacts:
-        contactName = contact['username']
-        contactIP = contact['ip']
-        if contactIP is "" or contactIP is None:
-            contactIP = "Offline"
-
-        # add the new values to the GUI
-        ConnectionsList[0].insert(END, contactName)
-        ConnectionsList[1].insert(END, contactIP)
-
-# update the global selected IP address
-def updateSelectedContact():
-
-    selectedName = ConnectionsList[0].get(ACTIVE)
-    selectedIP = ConnectionsList[1].get(ACTIVE)
-
-    for contact in Contacts:
-        if contact['ip'] == selectedIP and contact['username'] == selectedName:
-            return contact
-    return None
-
-#method is called by a ConnectionsList list box when the selection is changed. Updates other accordingly
-def syncListSelections(evt=None, listIndex=0):
-
-    otherindex = (listIndex +1) % 2
-    toset = ConnectionsList[listIndex].curselection()
-    ConnectionsList[otherindex].activate(toset[0])
-
-def closeProgram():
-    reactor.stop()
-
-#Set up the GUI and containers, frames, lists, etc. before running the program loop
-def initializeGUI():
-    global ConnectionsList
-    global chatWindow
-    global textEntry
-    #set up the main window
-    root = Tk()
-    root.title("Encrypted P2P Chat GUI")
-    root.protocol('WM_DELETE_WINDOW', closeProgram)
-
-    mainFrame = Frame(root)
-    mainFrame.pack()
-
-    ConnectionsList.append(Listbox(mainFrame, selectmode=SINGLE))
-    ConnectionsList.append(Listbox(mainFrame, selectmode=SINGLE))
-
-    ConnectionsList[0].grid(row=0, column=0)
-    ConnectionsList[1].grid(row=0, column=1)
-
-    ConnectionsList[0].bind("<<ListboxSelect>>", lambda e:syncListSelections(e, listIndex=0))
-    ConnectionsList[1].bind("<<ListboxSelect>>", lambda e:syncListSelections(e, listIndex=1))
-
-    #set up chat window with scroll bar
-    chatTextFrame = Frame(root)
-
-    scrollbar = Scrollbar(chatTextFrame)
-    scrollbar.pack(side=RIGHT, fill=Y)
-
-    chatWindow = Text(chatTextFrame, height=8, state=DISABLED)
-    chatWindow.pack(side=LEFT, expand=YES, fill=BOTH)
-
-    scrollbar.config(command=chatWindow.yview)
-    chatWindow.config(yscrollcommand=scrollbar.set)
-
-    chatTextFrame.pack(expand=YES, fill=BOTH)
-
-    #set up user text field for input
-    chatEntryFrame = Frame(root)
-
-    scrollbar2 = Scrollbar(chatEntryFrame)
-    scrollbar2.pack(side=RIGHT, fill=Y)
-
-    textEntry = Text(chatEntryFrame, height=2)
-    textEntry.pack(side=LEFT, expand=YES, fill=BOTH)
-
-    scrollbar2.config(command=textEntry.yview)
-
-    chatEntryFrame.pack(expand=YES, fill=BOTH)
-
-    #Bind key events to method calls
-    textEntry.bind("<Key>", sendChatMessage)
-    textEntry.bind("<KeyRelease>", clearText)
+            gui.chatWindowPrintText(message)
 
 
-    #set up buttons and their method calls
-    refreshButton = Button(root, text="Refresh List", command=refreshAvailIP)
-    refreshButton.pack(side=LEFT)
-
-    exitButton = Button(root, text="Exit Program", command=closeProgram)
-    exitButton.pack(side=RIGHT)
-
-    #connectButton = Button(root, text="Connect", command=None)#connectToIP)
-    #connectButton.pack(side=RIGHT)
-
-    return root
-
-#set up the gui root and connect it to the reactor
-if not args.nogui:
-    root = initializeGUI()
-    tksupport.install(root)
 
 #Will automatically refresh the contacts every minute
 if args.refresh:
